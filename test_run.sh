e8b4ed787d7884cb9adbef1de01b41a41cd9afab
qichang.dong <qichang.dong@videt.cn>
2021-07-20 13:13:44 +0800

    update

---
+++ test_run.sh
@@ -0,0 +1,5 @@
+sh ./tools/dist_test.sh \
+    configs/kie/sdmgr/sdmgr_novisual_60e_wildreceipt.py \
+    /data2/models_ie/mmocr/sdmgr_novisual_60e_wildreceipt_20210517-a44850da.pth \
+    1 \
+    --eval macro_f1
