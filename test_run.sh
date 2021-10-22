e7b00747ecd07f66e839c2a5d54606cc0ea8b026
qichang.dong <qichang.dong@videt.cn>
2021-10-22 15:36:13 +0800

    u

--- test_run.sh
+++ test_run.sh
@@ -1,5 +1,4 @@
-sh ./tools/dist_test.sh \
+sh tools/kie_test_imgs.sh \
     configs/kie/sdmgr/sdmgr_novisual_60e_wildreceipt.py \
     /data2/models_ie/mmocr/sdmgr_novisual_60e_wildreceipt_20210517-a44850da.pth \
-    1 \
-    --eval macro_f1
+    /data2/models_ie/mmocr/output
